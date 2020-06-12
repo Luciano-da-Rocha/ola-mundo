304d5a9a884a0ba97c46cb2d7060cdc273d32e30
Luciano da Rocha <66585551+Luciano-da-Rocha@users.noreply.github.com>
2020-06-12 00:19:07 -0300

    To quase me acostumando...
    Jaja domino isso aqui!

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,4 @@ print(olamundo)
 helloworld='Olá mundo!'
 print(helloworld)
 #AH muleque, vou testar um pull, editando aqui num comentario
+#to apanhando ainda, mas ja to me acostumando, GitHub e Git parecem serem bem uteis!
